5f2863aa3d49e13318be3a6465ff0b2fc041d34e
出口 正美 <masami.deguchi@gmail.com>
2021-02-22 03:29:32 +0900

    add ejs

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 var http = require("http");
+var fs = require("fs");
+var ejs = require("ejs");
+
+const index_page = fs.readFileSync("./index.ejs", "utf8");
 
 //create a server object:
 http
-  .createServer(function(req, res) {
-    res.write("Hello World!"); //write a response to the client
+  .createServer(function (req, res) {
+    var content = ejs.render(index_page);
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.write(content); //write a response to the client
     res.end(); //end the response
   })
   .listen(8080); //the server object listens on port 8080
